719f10732992c55f6f47d0b0a65365d32312232c
Max Mamis <max@citizen.com>
2019-09-06 14:51:29 -0400

    Make project work with Carthage

---
+++ BugBattle-iOS-SDK/BugBattle-iOS-SDK/BugBattle_iOS_SDK.h
@@ -0,0 +1,19 @@
+//
+//  BugBattle_iOS_SDK.h
+//  BugBattle-iOS-SDK
+//
+//  Created by max on 9/6/19.
+//  Copyright © 2019 BugBattle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BugBattle_iOS_SDK.
+FOUNDATION_EXPORT double BugBattle_iOS_SDKVersionNumber;
+
+//! Project version string for BugBattle_iOS_SDK.
+FOUNDATION_EXPORT const unsigned char BugBattle_iOS_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BugBattle_iOS_SDK/PublicHeader.h>
+
+
